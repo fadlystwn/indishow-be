2a855a6c1fb8416e2a0e29e9fd069e5693ae020e
fadlystwn <fadlystwn@gmail.com>
2024-04-09 10:53:31 +0700

    Add validation tests for Artist model

--- spec/models/artist_spec.rb
+++ spec/models/artist_spec.rb
@@ -1,26 +1,44 @@
-
 require 'rails_helper'
 
 RSpec.describe Artist, type: :model do
-  # Set up a valid artist instance for testing
   let(:artist) { build(:artist) }
 
-  # Test that the factory is valid
   it 'has a valid factory' do
     expect(artist).to be_valid
   end
 
-  # Test that the name attribute is set correctly
+  it 'is invalid without a name' do
+    artist.name = nil
+    expect(artist).to be_invalid
+    expect(artist.errors[:name]).to include("can't be blank")
+  end
+
+  it 'is invalid without a genre' do
+    artist.genre = nil
+    expect(artist).to be_invalid
+    expect(artist.errors[:genre]).to include("can't be blank")
+  end
+
+  it 'is invalid without a bio' do
+    artist.bio = nil
+    expect(artist).to be_invalid
+    expect(artist.errors[:bio]).to include("can't be blank")
+  end
+
+  it 'is invalid with a bio less than 20 characters' do
+    artist.bio = 'Short bio'
+    expect(artist).to be_invalid
+    expect(artist.errors[:bio]).to include("is too short (minimum is 20 characters)")
+  end
+
   it 'has a name' do
     expect(artist.name).to eq('Nirvana')
   end
 
-  # Test that the genre attribute is set correctly
   it 'has a genre' do
     expect(artist.genre).to eq('Grunge')
   end
 
-  # Test that the bio attribute is set correctly
   it 'has a bio' do
     expect(artist.bio).to eq('Nirvana was an American rock band formed in 1987 in Aberdeen, Washington. Founded by lead singer and guitarist Kurt Cobain and bassist Krist Novoselic, the band went through a succession of drummers before recruiting Dave Grohl in 1990. Nirvana\'s success popularized alternative rock, and they were often referenced as the figurehead band of Generation X.')
   end
