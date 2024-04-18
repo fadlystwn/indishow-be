3b90d1266eb9604f50e4d859afa90b988a6c484c
fadlystwn <fadlystwn@gmail.com>
2024-04-18 17:38:36 +0700

    Update schema version in db/schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_04_18_091319) do
+ActiveRecord::Schema[7.1].define(version: 2024_04_18_103726) do
   create_table "albums", force: :cascade do |t|
     t.string "title"
     t.date "release_date"
