ebf2f2bd2398f4428fb394a83a197fac694dcd75
fadlystwn <fadlystwn@gmail.com>
2024-04-18 16:14:09 +0700

    fix schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_04_12_095402) do
+ActiveRecord::Schema[7.1].define(version: 2024_04_18_091319) do
   create_table "albums", force: :cascade do |t|
     t.string "title"
     t.date "release_date"
@@ -24,15 +24,6 @@ ActiveRecord::Schema[7.1].define(version: 2024_04_12_095402) do
     t.string "name"
     t.string "genre"
     t.string "bio"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "users", force: :cascade do |t|
-    t.string "name"
-    t.string "email"
-    t.string "password_digest"
-    t.string "phone_number"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
