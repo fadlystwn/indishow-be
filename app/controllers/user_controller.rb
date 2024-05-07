48d83b7f0dba29f8b68e9780e216a0f6998dd96f
fadlystwn <fadlystwn@gmail.com>
2024-05-07 20:19:22 +0700

    Refactor UsersController and User model to use API versioning and update associations

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,5 +1,4 @@
-class UsersController < ApplicationController
-
+class UserController < ApplicationController
   def create
     @user = User.new(user_params)
 
@@ -9,7 +8,14 @@ class UsersController < ApplicationController
       render json: @user.errors, status: :unprocessable_entity
     end
   end
-
+  #edit user
+  def update
+    @user = User.find(params[:id])
+    if@user.update(user_params)
+      render json: @user,status :created
+    else
+      render json: @user.errrors, status: :unprocessable_entity
+  end
   #destroy user
   def destroy
     @user = User.find_by(id: params[:id])
@@ -19,7 +25,6 @@ class UsersController < ApplicationController
       render json: {error: "User not found"}, status: :not_found
     end
   end
-
 
   private
   def user_params
